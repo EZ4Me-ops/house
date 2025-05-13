a8e2ba073765a04ff319cf2cb046b203249e4d3e
Zyro <Aditymenon16@gmail.com>
2025-05-13 18:57:03 +0400

    Add files via upload

---
+++ revision.py
@@ -0,0 +1,17 @@
+# # data types
+
+# # comments 
+# """"jsdcbdsbcmn
+# mnsdmsd cm
+# msdcmdn
+# """
+# """"boolens strings intergers floats  idk """
+# # name = "aditya" 
+# # age = 12  
+print ("hello my name is aditya and i am 12 years old sadly ")
+
+name = input("enter your name : ")
+age = input( "ënter your age: ")
+
+# concatenation
+print("HEllo" , name , "ÿou are happily " , age , "ÿears old.")do
